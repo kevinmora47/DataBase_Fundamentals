2fa8530808fefbabaff43ec1ce616eb2bd25d5d2
kevinmora47 <kevinmora47@gmail.com>
2020-10-12 22:31:27 -0600

    Task#2-Uncomment tables-12-10-2020

--- Scripts/platzi-blog.sql
+++ Scripts/platzi-blog.sql
@@ -2,11 +2,12 @@
 USE platziblog;
 
 -- Creation of table structure and atributes.
--- CREATE TABLE categorias(
---     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
---     categoria VARCHAR(30)
--- );
--- CREATE TABLE etiquetas(
---     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
---     nombre_etiqueta VARCHAR(30)
--- );
+CREATE TABLE categorias(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    categoria VARCHAR(30)
+);
+
+CREATE TABLE etiquetas(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre_etiqueta VARCHAR(30)
+);

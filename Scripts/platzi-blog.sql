d8843af7b39ce47b14db959c3d17cd4a0fd17f3f
kevinmora47 <kevinmora47@gmail.com>
2020-10-12 22:30:29 -0600

    Task#1-Creation of structure-12-10-2020

---
+++ Scripts/platzi-blog.sql
@@ -0,0 +1,12 @@
+-- We are using platzi blog.
+USE platziblog;
+
+-- Creation of table structure and atributes.
+-- CREATE TABLE categorias(
+--     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     categoria VARCHAR(30)
+-- );
+-- CREATE TABLE etiquetas(
+--     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+--     nombre_etiqueta VARCHAR(30)
+-- );
